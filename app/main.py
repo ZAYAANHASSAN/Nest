af139a1e3f3af1f4dd1a5e9de1b9812c89ef68df
zayaanhassan <zayaanhassan918@gmail.com>
2025-06-25 23:24:34 +0530

    debug5

--- app/main.py
+++ app/main.py
@@ -7,10 +7,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World!"}
-
 app.include_router(student_routes.router)
 app.include_router(class_routes.router)
 
